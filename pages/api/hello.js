56e15604c88349fc11e3ff85c666410913aeeb58
yjtay <yjtay@axrail.com>
2023-05-09 01:48:02 +0800

    Added solution for subtask 1

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,18 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+import { createHash } from "crypto";
+
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+export default async function handler(req, res) {
+  try {
+    const hash = createHash("sha256")
+      .update(new Date().getMilliseconds().toString())
+      .digest("hex");
+    await sleep(1000);
+    res.status(200).json({ hash });
+  } catch (error) {
+    res.json(error);
+    res.status(405).end();
+  }
 }
