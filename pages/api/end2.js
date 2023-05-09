35cb93063c91e663d450ac5794fea8538088d5b3
yjtay <yjtay@axrail.com>
2023-05-09 22:27:00 +0800

    API endpoint code optimization for performance

--- pages/api/end2.js
+++ pages/api/end2.js
@@ -2,19 +2,19 @@ import { getProtocol } from "../../utils/vercel-utils";
 
 export default async function handler(req, res) {
   try {
-    while (true) {
-      //loop until lastChar is an odd number
+    const intervalId = setInterval(async () => {
       const response = await fetch(
         getProtocol() + req.headers.host + "/api/hello"
       );
       const data = await response.json();
-      let lastChar = parseInt(data.hash.charAt(data.hash.length - 1));
+      const lastChar = parseInt(data.hash.charAt(data.hash.length - 1));
       if (!isNaN(lastChar) && lastChar % 2 !== 0) {
-        return res.status(200).json({ message: "Success" });
+        clearInterval(intervalId);
+        res.status(200).json({ message: "Success" });
       }
-    }
+    }, 100);
   } catch (error) {
-    console.log(error);
-    res.status(500).send({ error });
+    console.error(error);
+    res.status(500).json({ error });
   }
 }
--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,15 +1,17 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 import { createHash } from "crypto";
+import { promisify } from "util";
 
 const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+const setTimeoutPromise = promisify(setTimeout);
 
 export default async function handler(req, res) {
   try {
     const hash = createHash("sha256")
       .update(new Date().getMilliseconds().toString())
       .digest("hex");
-    await sleep(1000);
+    await setTimeoutPromise(1000);
     res.status(200).json({ hash });
   } catch (error) {
     res.json(error);
