723c9d44495b474bf8221be3520cd48468511c47
yjtay <yjtay@axrail.com>
2023-05-09 22:49:56 +0800

    API endpoint 1 code optimization

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,18 +1,20 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 import { createHash } from "crypto";
+import { promisify } from "util";
 
-const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+const setTimeoutPromise = promisify(setTimeout);
 
 export default async function handler(req, res) {
   try {
     const hash = createHash("sha256")
       .update(new Date().getMilliseconds().toString())
       .digest("hex");
-    await sleep(1000);
+
+    await setTimeoutPromise(1000); // wait for 1 second
+
     res.status(200).json({ hash });
   } catch (error) {
-    res.json(error);
-    res.status(500).end();
+    res.status(500).json({ message: "Internal Server Error" });
   }
 }
